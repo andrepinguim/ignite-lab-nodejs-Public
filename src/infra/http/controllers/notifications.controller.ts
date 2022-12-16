55bda477cf292abc21640cff0f233a8db5eb714b
André Vasquez <andre.vazquez@copastur.com.br>
2022-12-15 23:36:14 -0300

    creating view model for notification

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-viewmodel';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -10,10 +11,14 @@ export class NotificationsController {
   async create(@Body() body: CreateNotificationBody) {
     const { recipientId, content, category } = body;
 
-    const { } = await this.sendNotification.execute({
+    const { notification } = await this.sendNotification.execute({
       content,
       category,
       recipientId
     });
+
+    return {
+      notification: NotificationViewModel.toHttp(notification)
+    }
   }
 }
